7f2cf9fd96762fe2fc001b652cfe5668cfb4b89f
sekararumk <sekararumk21@gmail.com>
2022-05-26 22:48:28 +0700

    update

---
+++ list.js
@@ -0,0 +1,238 @@
+const listData = [
+    {
+        title: "Not Started",
+        description: "Way to Town of Colors",
+        cover: "/images/cover-01.png",
+        cards: [
+            {
+                title: "Not Started",
+                due: null,
+                content: `
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,
+
+                - Basic introduction of the company and objectives for the project
+                - Collect all necessary contact details from freelancer
+                - Send out/receive contract (if necessary)
+                - Send/email a welcome packet (if necessary) 
+                - Freelancer introduction email or call to other team members
+                - Set up communication (email, software, etc)
+                `,
+                tags: [
+                    {
+                        title: "Website",
+                        bgClass: "bg-yellow-100",
+                        textClass: "text-yellow-100"
+                    },
+                    {
+                        title: "UX",
+                        bgClass: "bg-green-100",
+                        textClass: "text-green-100"
+                    },
+                    {
+                        title: "UI",
+                        bgClass: "bg-red-100",
+                        textClass: "text-red-100"
+                    },
+                    {
+                        title: "Wireframe",
+                        bgClass: "bg-purple-100",
+                        textClass: "text-purple-100"
+                    },
+                ],
+                avatar: null
+            },
+            {
+                title: "Image Rotaion Define",
+                due: null,
+                content: `
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,
+
+                1. Basic introduction of the company and objectives for the project
+                2. Collect all necessary contact details from freelancer
+                3. Send out/receive contract (if necessary)
+                `,
+                tags: [],
+                avatar: null
+            }
+        ]
+    },
+    {
+        title: "In Process",
+        description: "Way to Town of Colors",
+        cover: "/images/cover-02.png",
+        cards: [
+            {
+                title: "Image Ration Define",
+                due: null,
+                content: `
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,
+
+                - Basic introduction of the company and objectives for the project
+                `,
+                tags: [],
+                avatar: '/images/avatar-01.png'
+            },
+            {
+                title: "Image Rotation Define",
+                due: null,
+                content: `
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,
+
+                1. Basic introduction of the company and objectives for the project
+                2. Collect all necessary contact details from freelancer
+                3. Send out/receive contract (if necessary)
+                `,
+                tags: [
+                    {
+                        title: "Website",
+                        bgClass: "bg-yellow-100",
+                        textClass: "text-yellow-100"
+                    },
+                    {
+                        title: "UX",
+                        bgClass: "bg-green-100",
+                        textClass: "text-green-100"
+                    }
+                ],
+                avatar: '/images/avatar-02.png'
+            },
+            {
+                title: "Something New is Coming",
+                due: null,
+                content: `
+                ### !!IMPORTANT
+
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,
+
+                1. Basic introduction of the company and objectives for the project
+                2. Collect all necessary contact details from freelancer
+                3. Send out/receive contract (if necessary)
+                `,
+                tags: [
+                    {
+                        title: "Website",
+                        bgClass: "bg-yellow-100",
+                        textClass: "text-yellow-100"
+                    },
+                    {
+                        title: "UX",
+                        bgClass: "bg-green-100",
+                        textClass: "text-green-100"
+                    }
+                ],
+                avatar: '/images/avatar-02.png'
+            }
+        ]
+    },
+    {
+        title: "In Review",
+        description: "Way to Town of Colors",
+        cover: "/images/cover-03.png",
+        cards: [
+            {
+                title: "Image Ration Define",
+                due: null,
+                content: `
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,
+
+                - Basic introduction of the company and objectives for the project
+                - Collect all necessary contact details from freelancer
+                - Send out/receive contract (if necessary)
+                `,
+                tags: [
+                    {
+                        title: "Website",
+                        bgClass: "bg-yellow-100",
+                        textClass: "text-yellow-100"
+                    },
+                    {
+                        title: "UX",
+                        bgClass: "bg-green-100",
+                        textClass: "text-green-100"
+                    },
+                    {
+                        title: "UI",
+                        bgClass: "bg-red-100",
+                        textClass: "text-red-100"
+                    },
+                    {
+                        title: "Wireframe",
+                        bgClass: "bg-purple-100",
+                        textClass: "text-purple-100"
+                    },
+                ],
+                avatar: '/images/avatar-04.png'
+            },
+            {
+                title: "Image Rotation Define",
+                due: null,
+                content: `
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,
+
+                1. Basic introduction of the company and objectives for the project
+                2. Collect all necessary contact details from freelancer
+                3. Send out/receive contract (if necessary)
+                `,
+                tags: [
+                    {
+                        title: "Website",
+                        bgClass: "bg-yellow-100",
+                        textClass: "text-yellow-100"
+                    },
+                    {
+                        title: "UX",
+                        bgClass: "bg-green-100",
+                        textClass: "text-green-100"
+                    }
+                ],
+                avatar: '/images/avatar-05.png'
+            }
+        ]
+    },
+    {
+        title: "Completed",
+        description: "Way to Town of Colors",
+        cover: "/images/cover-04.png",
+        cards: [
+            {
+                title: "Not Started",
+                due: null,
+                content: `
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,
+
+                - Basic introduction of the company and objectives for the project
+                - Collect all necessary contact details from freelancer
+                - Send out/receive contract (if necessary)
+                - Send/email a welcome packet (if necessary) 
+                - Freelancer introduction email or call to other team members
+                - Set up communication (email, software, etc)
+                `,
+                tags: [
+                    {
+                        title: "Website",
+                        bgClass: "bg-yellow-100",
+                        textClass: "text-yellow-100"
+                    },
+                    {
+                        title: "UX",
+                        bgClass: "bg-green-100",
+                        textClass: "text-green-100"
+                    },
+                    {
+                        title: "UI",
+                        bgClass: "bg-red-100",
+                        textClass: "text-red-100"
+                    },
+                    {
+                        title: "Wireframe",
+                        bgClass: "bg-purple-100",
+                        textClass: "text-purple-100"
+                    },
+                ],
+                avatar: null
+            }
+        ]
+    }
+]
+
+export default listData
